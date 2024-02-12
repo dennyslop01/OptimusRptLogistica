ea47ffeb84aad5b3ff3a30fc6190a553e247ae49
Erik Perez <erikxp@gmail.com>
2024-02-12 18:25:20 -0300

    add logo global rpt at login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,16 +1,17 @@
 import { useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Alert, Button, Grid, TextField, Typography } from '@mui/material';
+import { Alert, Button, Grid, TextField, Typography, Box } from '@mui/material';
 import { ConstructionOutlined, Microsoft } from '@mui/icons-material';
 import { AuthLayout } from '../layout/AuthLayout';
-import { useForm } from '../../hooks';
-import { startMicrosoftSignIn, startLoginWithEmailPassword } from '../../store/auth';
+import { useForm } from '@/hooks';
+import { startMicrosoftSignIn, startLoginWithEmailPassword } from '@/store/auth';
 import { getAuth } from 'firebase/auth';
 import { getEnvVariables } from '../../helpers';
+import { LOGORPT } from '@/constants.js';
 
 export const LoginPage = () => {
 
-  const { status, errorMessage } = useSelector( state => state.auth );
+  const { status, errorMessage } = useSelector(state => state.auth);
 
   const dispatch = useDispatch();
   const { email, password, onInputChange } = useForm({
@@ -18,83 +19,94 @@ export const LoginPage = () => {
     password: ''
   });
 
-  const isAuthenticating = useMemo( () => status === 'checking', [status]);
+  const isAuthenticating = useMemo(() => status === 'checking', [status]);
 
-  const onSubmit = ( event ) => {
+  const onSubmit = (event) => {
     event.preventDefault();
-    dispatch( startLoginWithEmailPassword({ email, password }) );
+    dispatch(startLoginWithEmailPassword({ email, password }));
   }
 
   const onMicrosoftSignIn = () => {
-    dispatch( startMicrosoftSignIn() );
+    dispatch(startMicrosoftSignIn());
   }
 
   return (
-    <AuthLayout title="Login">
-      <form onSubmit={ onSubmit } className='animate__animated animate__fadeIn animate__faster'>
-          <Grid container>
-            <Grid item xs={ 12 } sx={{ mt: 2 }}>
-              <TextField 
-                label="Correo" 
-                type="email" 
-                placeholder='correo@gmail.com' 
-                fullWidth
-                name="email"
-                value={ email }
-                onChange={ onInputChange }
-              />
-            </Grid>
-
-            <Grid item xs={ 12 } sx={{ mt: 2 }}>
-              <TextField 
-                label="Contraseña" 
-                type="password" 
-                placeholder='Contraseña' 
-                fullWidth
-                name="password"
-                value={ password }
-                onChange={ onInputChange }
-              />
-            </Grid>
-
-            <Grid 
-              container
-              display={ !!errorMessage ? '': 'none' }
-              sx={{ mt: 1 }}>
-              <Grid 
-                  item 
-                  xs={ 12 }
-                >
-                <Alert severity='error'>{ errorMessage }</Alert>
-              </Grid>
-            </Grid>
-            
-            <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
-              <Grid item xs={ 12 } sm={ 6 }>
-                <Button
-                  disabled={ isAuthenticating }
-                  type="submit" 
-                  variant='contained' 
-                  fullWidth>
-                  Login
-                </Button>
-              </Grid>
-
-              <Grid item xs={ 12 } sm={ 6 }>
-                <Button
-                   disabled={ isAuthenticating }
-                   variant='contained' 
-                   fullWidth
-                   onClick={ onMicrosoftSignIn }>
-                  <Microsoft />
-                  <Typography sx={{ ml: 1 }}>Office 365</Typography>
-                </Button>
-              </Grid>
-            </Grid>
-
+    <AuthLayout>
+      <form onSubmit={onSubmit} className='animate__animated animate__fadeIn animate__faster'>
+        <Grid item xs={12}>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              p: 2
+            }}
+          >
+            <img src={LOGORPT} alt="Logo" style={{ maxWidth: '100%', height: 'auto' }} />
+          </Box>
+        </Grid>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder='correo@gmail.com'
+              fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
+            />
           </Grid>
 
-        </form>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder='Contraseña'
+              fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
+            />
+          </Grid>
+
+          <Grid
+            container
+            display={!!errorMessage ? '' : 'none'}
+            sx={{ mt: 1 }}>
+            <Grid
+              item
+              xs={12}
+            >
+              <Alert severity='error'>{errorMessage}</Alert>
+            </Grid>
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12} sm={6}>
+              <Button
+                disabled={isAuthenticating}
+                type="submit"
+                variant='contained'
+                fullWidth>
+                Login
+              </Button>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <Button
+                disabled={isAuthenticating}
+                variant='contained'
+                fullWidth
+                onClick={onMicrosoftSignIn}>
+                <Microsoft />
+                <Typography sx={{ ml: 1 }}>Office 365</Typography>
+              </Button>
+            </Grid>
+          </Grid>
+
+        </Grid>
+
+      </form>
 
     </AuthLayout>
   )
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const LOGORPT = `${import.meta.env.VITE_ASSETS_BASE_URL}assets/images/logorpt.png`;
